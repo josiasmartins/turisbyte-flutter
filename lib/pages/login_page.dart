2f6fd9408bf0e2d54abe70f1d3960b168ecae711
josias.m.caitano <josiasmartinscaitano@gmail.com>
2023-10-22 11:59:12 -0300

    fix: fixed laout overflowed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -19,18 +19,18 @@ class MyApp extends StatelessWidget {
 
 class LoginPage extends StatelessWidget {
 
-Color hexToColor(String code) {
-  // Remove o caractere '#' (se presente)
-  if (code[0] == '#') {
-    code = code.substring(1);
+  Color hexToColor(String code) {
+    // Remove o caractere '#' (se presente)
+    if (code[0] == '#') {
+      code = code.substring(1);
+    }
+
+    // Adiciona um valor alpha de 255 (0xFF) e converte para inteiro
+    int colorInt = int.parse(code, radix: 16) + 0xFF000000;
+
+    // Cria e retorna o objeto Color
+    return Color(colorInt);
   }
-
-  // Adiciona um valor alpha de 255 (0xFF) e converte para inteiro
-  int colorInt = int.parse(code, radix: 16) + 0xFF000000;
-
-  // Cria e retorna o objeto Color
-  return Color(colorInt);
-}
 
   @override
   Widget build(BuildContext context) {
@@ -39,58 +39,56 @@ Color hexToColor(String code) {
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(16.0),
-              // child: Image.asset(
-              //   'assets/logo.png', // Substitua pelo caminho real da sua imagem
-              //   height: 100.0,
-              // ),
-              child: SvgPicture.asset(
-                "../../assets/images/logo_header.svg",
-                semanticsLabel: 'My SVG Image',
-                height: 100,
-                width: 70,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(16.0),
+                child: SvgPicture.asset(
+                  "../../assets/images/logo_header.svg",
+                  semanticsLabel: 'My SVG Image',
+                  height: 100,
+                  width: 70,
+                ),
               ),
-            ),
-            CustomInputField(
-              labelText: 'Email',
-              inputWidth: inputWidth,
-            ),
-            CustomInputField(
-              labelText: 'Senha',
-              obscureText: true,
-              inputWidth: inputWidth,
-            ),
-            CustomButton(
-              buttonText: "Entrar",
-              buttonColor: hexToColor("#FD0101"), // Cor do botão
-              onPressed: () {
-                // Adicione a lógica de autenticação aqui
-              },
-            ),
-            CustomButton(
-              buttonText: "Cadastrar-se",
-              buttonColor: Colors.black, // Cor do botão
-              onPressed: () {
-                // Adicione a lógica de autenticação aqui
-              },
-            ),
-            TextButton(
-              onPressed: () {
-                // Implemente a função de cadastro aqui
-              },
-              child: Text('Cadastrar-se'),
-            ),
-            Divider(
-              height: 20.0,
-              color: Colors.black,
-              thickness: 1.0,
-              indent: 16.0,
-              endIndent: 16.0,
-            ),
-          ],
+              CustomInputField(
+                labelText: 'Email',
+                inputWidth: inputWidth,
+              ),
+              CustomInputField(
+                labelText: 'Senha',
+                obscureText: true,
+                inputWidth: inputWidth,
+              ),
+              CustomButton(
+                buttonText: "Entrar",
+                buttonColor: hexToColor("#FD0101"), // Cor do botão
+                onPressed: () {
+                  // Adicione a lógica de autenticação aqui
+                },
+              ),
+              CustomButton(
+                buttonText: "Cadastrar-se",
+                buttonColor: Colors.black, // Cor do botão
+                onPressed: () {
+                  // Adicione a lógica de autenticação aqui
+                },
+              ),
+              TextButton(
+                onPressed: () {
+                  // Implemente a função de cadastro aqui
+                },
+                child: Text('Cadastrar-se'),
+              ),
+              Divider(
+                height: 20.0,
+                color: Colors.black,
+                thickness: 1.0,
+                indent: 16.0,
+                endIndent: 16.0,
+              ),
+            ],
+          ),
         ),
       ),
     );
